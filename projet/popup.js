44a19a5f583a8f5aa796923fabd1bbf43135a2d6
Sarah Watremet <sarah.watremet@gmail.com>
2022-01-18 16:58:49 +0100

    Work on API
    Button is now working - need to fix the API (using AJAX instead of Axios)

--- projet/popup.js
+++ projet/popup.js
@@ -1,74 +1,48 @@
-// let getQuote = document.getElementById("getQuote");
-
-// // chrome.storage.sync.get("color", ({ color }) => {
-// //   changeColor.style.backgroundColor = color;
-// // });
-
-// // When the button is clicked, inject setPageBackgroundColor into current page
-// getQuote.addEventListener("click", async () => {
-//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-//   chrome.scripting.executeScript({
-//     target: { tabId: tab.id },
-//     function: 
-//   });
-// });
-
-import axios from "axios";
-import { createSecureServer } from "http2";
-const api = "https://animechan.vercel.app/api/quotes";
-const errors = document.querySelector(".errors");
-// const apiTitle = "https://animechan.vercel.app/api/quotes/anime?title=naruto"
-// const apiCharacter = "https://animechan.vercel.app/api/quotes/character?name=saitama"
+// import axios from "axios";
+// const api = "https://animechan.vercel.app/api/quotes";
+// const errors = document.querySelector(".errors");
+// // const apiTitle = "https://animechan.vercel.app/api/quotes/anime?title=naruto"
+// // const apiCharacter = "https://animechan.vercel.app/api/quotes/character?name=saitama"
 // const loading = document.querySelector(".loading");
-const quote = document.querySelector(".quote");
-const character = document.querySelector(".character");
-const result = document.querySelector(".result-container");
-result.style.display = "none";
-loading.style.display = "none";
+// const quote = document.querySelector(".anime");
+// const character = document.querySelector(".character");
+// const result = document.querySelector(".result-container");
+// result.style.display = "none";
+// loading.style.display = "none";
 
-//grab the form (formulaire)
-const form = document.querySelector(".form-data");
-//grab the anime title
-const anime = document.querySelector(".anime");
+// // //grab the form (formulaire)
+// const form = document.querySelector(".form-data");
+// //grab the anime title
+// const anime = document.querySelector(".anime-title");
 
-//declare method to search by anime title
-const searchForTitle = async anime => {
-    loading.style.display = "block";
-    errors.textContent = "";
-    try{
-        const response = await axios.get(`${api}/${anime}`);
-        loading.style.display = "none";
-        quote.textContent = response.data.quote.value;
-        character.textContent = response.data.character.value;
-        result.style.display = "block";
-    }
-    catch (error){
-        result.style.display = "none";
-        loading.style.display = "none";
-        errors.textContent = "Oops looks like we can't find it :o";
-    }
-};
+// //declare method to search by anime title
+// const searchForTitle = async anime => {
+//     loading.style.display = "block";
+//     errors.textContent = "";
+//     try{
+//         const response = await axios.get(`${api}/${anime}`);
+//         console.log(response)
+//         loading.style.display = "none";
+//         quote.textContent = response.data.quote;
+//         character.textContent = response.data.character;
+//         result.style.display = "block";
+//     }
+//     catch (error){
+//         result.style.display = "none";
+//         loading.style.display = "none";
+//         errors.textContent = "Oops looks like we can't find it :o";
+//     }
+// };
 
 //declare a function to handle form submission
-const handleSubmit = async e => {
+function handleSubmit() {
     e.preventDefault();
-    searchForTitle(anime.value);
-    console.log(anime.value);
+    searchForTitle(anime);
+    console.log(anime);
 };
 
 //event listener
-form.addEventListener("submit", e => handleSubmit(e));
-
-
-
-
-
-
-
-
-// async function callApi(){
-// let call = await fetch('https://animechan.vercel.app/api/random')
-// .then(response => response.json())
-// .then(quote => console.log(quote))
-// }
+let button = document.getElementById("btn");
+button.addEventListener("click", function () {
+    console.log("HELLO");
+});
